18ce6f1e306521caa5f9daa93c2c16a71409224c
minhlucvan <toilatoi123>
2017-03-09 07:10:15 +0700

    modify counter module to work with redux-saga

--- src/routes/Counter/modules/counter.js
+++ src/routes/Counter/modules/counter.js
@@ -1,8 +1,14 @@
 // ------------------------------------
+// Dependentcies
+// ------------------------------------
+import { put, takeEvery } from 'redux-saga/effects'
+
+// ------------------------------------
 // Constants
 // ------------------------------------
 export const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
 export const COUNTER_DOUBLE_ASYNC = 'COUNTER_DOUBLE_ASYNC'
+export const COUNTER_DOUBLE = 'COUNTER_DOUBLE'
 
 // ------------------------------------
 // Actions
@@ -18,17 +24,9 @@ export function increment (value = 1) {
     returns a function for lazy evaluation. It is incredibly useful for
     creating async actions, especially when combined with redux-thunk! */
 
-export const doubleAsync = () => {
-  return (dispatch, getState) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        dispatch({
-          type    : COUNTER_DOUBLE_ASYNC,
-          payload : getState().counter
-        })
-        resolve()
-      }, 200)
-    })
+export function doubleAsync(){
+  return {
+    type: COUNTER_DOUBLE_ASYNC
   }
 }
 
@@ -38,11 +36,28 @@ export const actions = {
 }
 
 // ------------------------------------
+// Midlewares
+// ------------------------------------
+
+function*  doAsyncDouble(action){
+  yield new Promise(resolve => setTimeout(resolve, 200));
+  yield put({type: COUNTER_DOUBLE}); 
+}
+
+function* asyncDoubleSaga() {
+  yield takeEvery(COUNTER_DOUBLE_ASYNC, doAsyncDouble)
+}
+
+export const middlewares = {
+  asyncDoubleSaga
+}
+
+// ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
   [COUNTER_INCREMENT]    : (state, action) => state + action.payload,
-  [COUNTER_DOUBLE_ASYNC] : (state, action) => state * 2
+  [COUNTER_DOUBLE]       : (state, action) => state * 2
 }
 
 // ------------------------------------
