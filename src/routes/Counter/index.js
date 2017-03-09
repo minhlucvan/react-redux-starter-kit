bcb96135b8cf5a9651db10a5b5a7350aac06891d
minhlucvan <toilatoi123>
2017-03-09 08:59:16 +0700

    fixed bug multiple injecing

--- src/routes/Counter/index.js
+++ src/routes/Counter/index.js
@@ -1,6 +1,8 @@
 import { injectReducer } from '../../store/reducers'
 import { runSagas } from '../../store/middlewares'
 
+var injectedOnce = false;
+
 export default (store) => ({
   path : 'counter',
   /*  Async getComponent is only invoked when route matches   */
@@ -11,12 +13,16 @@ export default (store) => ({
       /*  Webpack - use require callback to define
           dependencies for bundling   */
       const Counter = require('./containers/CounterContainer').default
-      const {default: reducer, middlewares} = require('./modules/counter')
+      
+      /* just neeed to inject things in the first time  */
+      if ( !injectedOnce ){
+        const {default: reducer, middlewares} = require('./modules/counter')
 
-      /*  Add the reducer to the store on key 'counter'  */
-      injectReducer(store, { key: 'counter', reducer })
-      runSagas(store, middlewares)
-
+        /*  Add the reducer to the store on key 'counter'  */
+        injectReducer(store, { key: 'counter', reducer })
+        runSagas(store, middlewares)
+        injectedOnce = true
+      }
       /*  Return getComponent   */
       cb(null, Counter)
 
